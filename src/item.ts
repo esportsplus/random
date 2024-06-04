5ff3f3d29de16fd9231f2c16fb1d950d7852a80f
Israel Casillas Jr <ICJR@users.noreply.github.com>
2024-06-03 23:45:36 -0700

    chore: cleanup

--- src/item.ts
+++ src/item.ts
@@ -1,32 +1,27 @@
 import { rng } from '@esportsplus/crypto';
 
 
-let cache = new WeakMap();
-
-
 export default <T>(items: T[], weights?: number[]): T => {
     if (weights === undefined) {
         return items[ Math.floor(rng() * items.length) ];
     }
 
-    if (items.length !== weights.length) {
+    let n = items.length,
+        random = 0;
+
+    if (n !== weights.length) {
         throw new Error('Random: each item requires a weight');
     }
 
-    let total = cache.get(weights) || 0;
-
-    if (!total) {
-        for (let i = 0, n = weights.length; i < n; i++) {
-            total += weights[i];
-        }
-
-        cache.set(weights, total);
+    for (let i = 0; i < n; i++) {
+        random += weights[i];
     }
 
-    let current = 0,
-        random = rng() * total;
+    let current = 0;
 
-    for (let i = 0, n = items.length; i < n; i++) {
+    random *= rng();
+
+    for (let i = 0; i < n; i++) {
         current += weights[i];
 
         if (random <= current) {
